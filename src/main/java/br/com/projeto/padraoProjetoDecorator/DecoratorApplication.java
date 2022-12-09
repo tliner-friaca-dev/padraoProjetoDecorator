5dffa59ef41cd7fd34e6173c0a4eea779bb58648
tliner-friaca-dev <tlinerfc@hotmail.com>
2022-12-09 19:09:21 -0300

    first commit

---
+++ src/main/java/br/com/projeto/padraoProjetoDecorator/DecoratorApplication.java
@@ -0,0 +1,18 @@
+package br.com.projeto.padraoProjetoDecorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoratorApplication.class, args);
+
+		System.out.println("----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------");
+
+	}
+
+}
---
+++ src/test/java/br/com/projeto/padraoProjetoDecorator/DecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.padraoProjetoDecorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
